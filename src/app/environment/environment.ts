16b652f0d3dc56ac9b85e7263e56c1b36274fab4
Prashant <prashantsaransingh@gmail.com>
2023-07-31 13:04:32 +0530

    removed extra code

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+  production: false,
 
 
 
@@ -7,8 +7,8 @@ export const environment = {
     cityWeatherData1: 'https://api.openweathermap.org/data/2.5/weather?q=',
     cityWeatherData2:'&units=metric&APPID=695ed9f29c4599b7544d0db5c211d499',
      // Base URL to fetch weather icon images
-    url: 'https://api.openweathermap.org/data/2.5/forecast?q=',
-    url2:'&units=metric&appid=0c9a8a0e76f7733e057b14fa8093c596',
+url: 'https://api.openweathermap.org/data/2.5/forecast?q=',
+url2:'&units=metric&appid=0c9a8a0e76f7733e057b14fa8093c596',
 
     cloudIconUrl1:'http://openweathermap.org/img/wn/',
     cloudIconUrl2:'@2x.png'
--- src/app/service/weather.ts
+++ src/app/service/weather.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from '../environment/environment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,41 +10,11 @@ import { environment } from '../environment/environment';
 export class WeatherService {
   constructor(private http: HttpClient) { }
 
-  getWeatherForCity(city:string): Observable<any>{
-    const path3=`${environment.url}`+`${city}`+`${environment.url2}`;
+  getWeatherForCity(city: string): Observable<any> {
+    const path3 = `${environment.url}` + `${city}` + `${environment.url2}`;
     return this.http.get(path3);
   }
 
-
-
-  
-  getWeatherForCity2(city: string): Observable<any> {
-
-
-    const path = `${environment.cityWeatherData1}` + `${city}` + `${environment.cityWeatherData2}`;
-    return this.http.get(path).pipe(
-      map((data: any) => {
-        if (data && data.weather && Array.isArray(data.weather) && data.weather.length > 0 && data.weather[0].icon) {
-          return {
-            ...data,
-            image: `${environment.cloudIconUrl1}` + `${data.weather[0].icon}` + `${environment.cloudIconUrl2}`// added the image url to this Json data by creating it
-          };
-        } else {
-          // If the expected structure is not present in the API response, return the data as it is
-          return data;
-        }
-      }),
-
-      catchError((error) => {
-        // Handle API error here if needed
-        console.error('API Error:', error);
-
-        return of(error);
-      })
-    );
-    this.getWeatherForCity2(city).subscribe();
-
-  }
 
 }
 
